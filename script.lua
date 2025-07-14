fcc36c0f02882611e210e1da1bf82b0af92e30df
bailey923 <151684237+bailey923@users.noreply.github.com>
2025-07-14 19:12:13 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,3 @@
-local model = script.Parent  -- the model this script is inside of
-local part = model:FindFirstChild("Part")  -- change "Part" to your part's name
-
-if part then
-    part:Destroy()
-else
-    warn("Part not found in model!")
-end
+print("I am the system of the game")
 
 
