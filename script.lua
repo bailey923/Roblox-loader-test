3f6fad299aea6a04c67a3e7c4b16bd2dcc61c5e0
bailey923 <151684237+bailey923@users.noreply.github.com>
2025-07-14 19:11:19 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,10 @@
-local module = require(game.Workspace.Part)
-module.destroy()  -- assuming the module has a function called 'destroy'
+local model = script.Parent  -- the model this script is inside of
+local part = model:FindFirstChild("Part")  -- change "Part" to your part's name
+
+if part then
+    part:Destroy()
+else
+    warn("Part not found in model!")
+end
+
 
